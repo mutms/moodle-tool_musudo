3da5b5105f9072d38d58ba4556e5366990b13f66
Petr Skoda <petr@mutms.org>
2025-06-07 14:13:44 +0200

    Fix CI

--- settings.php
+++ settings.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-/** @var admin_root $ADMIN */
-
 $beforesibling = has_capability('moodle/role:manage', context_system::instance()) ? 'defineroles' : null;
+
+/** @var admin_root $ADMIN */
 $ADMIN->add('roles', new admin_externalpage('tool_musudo_sudoers',
     get_string('sudoers', 'tool_musudo'),
     new moodle_url('/admin/tool/musudo/index.php'), 'moodle/site:config'), $beforesibling);
