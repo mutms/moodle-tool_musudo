61b39de0d521813f7cfafd13749a53e94c658d98
Petr Skoda <petr@mutms.org>
2026-08-08 20:01:47 +0000

    Prepare release

--- version.php
+++ version.php
@@ -28,13 +28,13 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_musudo';
-$plugin->version = 2026060545;
+$plugin->version = 2026080845;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 405];
 $plugin->incompatible = 500;
 $plugin->release = 'v4.5.13.01';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026060545,
+    'tool_mulib' => 2026080845,
     'tool_mfa' => 2024100700,
 ];
